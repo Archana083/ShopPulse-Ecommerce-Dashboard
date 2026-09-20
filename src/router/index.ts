d5565bb5e91bd81364bfcb5d926d63d02ac94ea7
Archana Maurya <126747758+Archana083@users.noreply.github.com>
2026-09-20 14:31:47 +0530

    feat: add ShopPulse data models and mock store

---
+++ src/router/index.ts
@@ -0,0 +1,3 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue'
+export default createRouter({history:createWebHistory(),routes:[{path:'/',component:App}]})
