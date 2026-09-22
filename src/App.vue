78127f931cad0e8038c1d3ee39a24585690c1d60
Archana Maurya <126747758+Archana083@users.noreply.github.com>
2026-09-22 18:11:13 +0530

    Use standalone layout for authentication pages

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import AppLayout from './layouts/AppLayout.vue'
+
+const route = useRoute()
+const isAuthPage = computed(() => ['/login', '/register', '/forgot-password'].includes(route.path))
 </script>
-<template><AppLayout /></template>
+<template>
+  <AppLayout v-if="!isAuthPage" />
+  <RouterView v-else />
+</template>
